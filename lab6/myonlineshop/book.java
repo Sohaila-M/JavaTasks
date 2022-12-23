a56f99e894b7c08e3a78e4cce40969e784eee180
Sohaila-M <64363349+Sohaila-M@users.noreply.github.com>
2022-12-23 14:35:33 +0200

    Update book.java

--- lab6/myonlineshop/book.java
+++ lab6/myonlineshop/book.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package myonlineshop;
 
-/**
- *
- * @author options
- */
-/*public class book extends product {*/
+
+
 public class book implements product {
    public int price(){
            return 10;}
